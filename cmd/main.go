e7051ec2630dc5eb18d8f6e09bbd68ccb8c94244
Xercoy <corey@prak.me>
2016-06-26 09:35:55 -0700

    Added help flag to display usage information.

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/xercoy/blobs"
 	"os"
 )
@@ -9,15 +10,23 @@ import (
 func main() {
 	var unit, dest, fmtStr string
 	var amount int
-	var random bool
+	var random, help bool
 
 	flag.StringVar(&unit, "unit", "MB", "Unit of space for the glob.")
 	flag.IntVar(&amount, "amount", 1, "Number of files to be created.")
 	flag.StringVar(&dest, "dest", "./", "Destination of created globs.")
 	flag.StringVar(&fmtStr, "o", "%d.dat", "Format specifier for blobs.")
 	flag.BoolVar(&random, "random", false, "Random number of blobs, 1 - amount")
+	flag.BoolVar(&help, "help", false, "Displays flag attributes & usage information.")
 
 	flag.Parse()
+	//	flag.CommandLine.SetOutput(os.Stdout)
+
+	if help {
+		fmt.Println("Blobs 1.0\n\n Usage: blobs <options> <options>...")
+		flag.PrintDefaults()
+		return
+	}
 
 	runner := blobs.NewRunner(os.Stdin, dest, unit, fmtStr, amount, random)
 
